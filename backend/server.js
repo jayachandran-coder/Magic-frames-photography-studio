ae31507116687f180abeec118128d8d862136589
jayachandran <jayachandran.ffw@gmail.com>
2026-04-02 14:10:15 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-const cors = require('cors');
 
 app.use(cors({
   origin: 'https://magic-frames-photography-studio.vercel.app' // ← your vercel URL
